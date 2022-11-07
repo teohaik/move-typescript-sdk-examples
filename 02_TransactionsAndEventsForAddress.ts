16b3d7ab5befc91e5d18528f0e8c1440302013ea
Teo Chaikalis <teo@mystenlabs.com>
2022-11-07 15:59:37 +0200

    added transactions and events

---
+++ 02_TransactionsAndEventsForAddress.ts
@@ -0,0 +1,40 @@
+/**
+ * Retrieves all Transactions performed by a given address and fetches the events emitted from the 1st transaction
+ */
+import { JsonRpcProvider } from '@mysten/sui.js';
+
+let message: string = 'Hello, Sui user!';
+console.log(message);
+
+const myAddress = '0x3e68b87e765d62a8c7ab8c2fbb284ad72d750e17'; //Example Address
+
+const provider = new JsonRpcProvider('https://fullnode.devnet.sui.io:443');
+
+provider.getTransactionsForAddress(myAddress)
+    .then(function (transactions) {
+        console.log("Transactions for Address " +myAddress);
+        console.log(transactions);
+        if(transactions.length > 0) {
+            getEventsForTransaction(transactions);
+        }
+    });
+
+function getEventsForTransaction(transactions: any) {
+    var transactionToGetEvents = transactions[0];
+    const txEvents = provider.getEventsByTransaction(
+        transactionToGetEvents
+    ).then(function (events) {
+        console.log("Events for transaction " + transactionToGetEvents);
+        console.log(events);
+    });
+}
+
+const senderEvents =  provider.getEventsBySender(
+    myAddress
+  ).then(function (events) {
+    console.log("Events by sender "+myAddress);
+    console.log(events);
+} );
+
+
+
