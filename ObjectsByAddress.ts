757d0f70ac54bd08e8ddca17b83c629c4cae4b51
Teo Chaikalis <teo@mystenlabs.com>
2022-11-07 15:42:38 +0200

    added gitignore

--- ObjectsByAddress.ts
+++ ObjectsByAddress.ts
@@ -4,19 +4,17 @@ import { JsonRpcProvider } from '@mysten/sui.js';
 let message: string = 'Hello, Sui!';
 console.log(message);
 
-const myAddress = '0x788a511738ad4ab1d7f769b49076d9c7b272826c';
+const myAddress = '0x3e68b87e765d62a8c7ab8c2fbb284ad72d750e17';
 
 
 const provider = new JsonRpcProvider('https://fullnode.devnet.sui.io:443');
 const objects = provider.getObjectsOwnedByAddress(
     myAddress
 ).then(function (res) {
-    console.log('results');
-  //  console.log(res);
-
-    console.log(' Objects Owned By Address'+myAddress +" :");
-    res.forEach(a => {
-        console.log('Object id : ' + a.objectId + "Type:  " + a.type);
+    console.log('Results:');
+    console.log('----- Objects Owned By Address: '+myAddress +" ------------------- :");
+    res.forEach(obj => {
+        console.log('Object id : ' + obj.objectId + " - Type:  " + obj.type);
     });
 
 });
