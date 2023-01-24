e37faceae9afb9632558f38ac4472528a5453b91
Theodore Chaikalis <teo@mystenlabs.com>
2023-01-24 14:56:12 +0200

    add more examples

--- 03_TransferObjects.ts
+++ 03_TransferObjects.ts
@@ -13,3 +13,15 @@ provider.requestSuiFromFaucet(addr)
         .then(function(result){
             console.log("Request finished with result: "+ result);
         })
+
+
+
+const keypair2 = new Ed25519Keypair();
+
+console.log(" public key base 64 ", keypair2.getPublicKey().toBase64());
+console.log(" public key toString ", keypair2.getPublicKey().toString());
+
+console.log(" public key to Sui Address ", keypair2.getPublicKey().toSuiAddress());
+
+console.log("Exporter keypair", keypair2.export());
+
