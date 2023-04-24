aa5818559062efb12d15a6d05105272d47db9cfe
Theodore Chaikalis <teo@mystenlabs.com>
2023-04-24 16:03:03 +0300

    fix getObjects methods

--- 01_ObjectsByAddress.ts
+++ 01_ObjectsByAddress.ts
@@ -1,23 +1,28 @@
 /**
  * Retrieves all Objects owned by a given address
  */
-import {JsonRpcProvider, Network} from '@mysten/sui.js';
+import {Connection, JsonRpcProvider} from "@mysten/sui.js";
 
-const provider = new JsonRpcProvider(Network.DEVNET);
+
+const connOptions = new Connection({
+    fullnode: 'https://suifrens-rpc.testnet.sui.io:443',
+});
+
+let provider = new JsonRpcProvider(connOptions);
 
 const capyArray = [
-    "97c87130059f483c67540197f788728be6890c29",
-    "e3a0240465103d7d5472eb53312d04368df2e36d",
-    "95a32be2f7ae6d68f91b0e85898db3a4df44100c"
+    "0x13008cab6607258afe42267f3c24839615dd5da123e914b5062e5a18ce795037",
+    "0x182dbc757a8e6387e100a30e4fb2de0167b2e30dcea55011da4500548ee79171",
+    "0x224ee50f6591acddbc6b9c5f83b9c58779a641f06562b7dbe37232d0cb29f5a2"
 ]
 
 
 console.log("Getting Multiple Objects with Batch Request");
 
 console.log("Batch array = ", capyArray);
-provider.getObjectBatch(
-    capyArray
-).then(function (res){
+provider.multiGetObjects( {
+    ids:capyArray
+} ).then(function (res){
     console.log("batch response");
     for(let i =0; i< res.length; i++){
         let obj = res[i];
@@ -26,17 +31,15 @@ provider.getObjectBatch(
 });
 
 
+const myAddress = '0x6cd789e6e45489fc61959e0aaa57e573f76c57d05c3c376684f6810e899ecc37'; //Example Address
 
-const myAddress = '0x3e68b87e765d62a8c7ab8c2fbb284ad72d750e17'; //Example Address
 
-
-const objects = provider.getObjectsOwnedByAddress(
-    myAddress
+const objects = provider.getOwnedObjects( {owner:  myAddress }
 ).then(function (res) {
     console.log('Results:');
-    console.log('----- Objects Owned By Address: '+myAddress +" ------------------- :");
-    res.forEach(obj => {
-        console.log('Object id : ' + obj.objectId + " - Type:  " + obj.type);
+    console.log('----- Objects Owned By Address: ' +myAddress +" ------------------- :");
+    res.data.forEach(obj => {
+        console.log('Object id : ' + obj.data.objectId );
     });
     console.log('Results END--------------------------');
 });
