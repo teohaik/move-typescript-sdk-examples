6af81cbe17de29cf02e1ed7a9bb982c2082f227c
Theodore Chaikalis <teo@mystenlabs.com>
2023-11-22 16:18:39 +0200

    add more examples on Dynamic field reads

--- 09_DynamicFieldReader.ts
+++ 09_DynamicFieldReader.ts
@@ -1,5 +1,5 @@
 import {getFullnodeUrl, SuiClient, SuiParsedData} from "@mysten/sui.js/client";
-import {SuiMoveObject} from "@mysten/sui.js";
+import { SuiMoveObject } from "@mysten/sui.js/src/types";
 
 const client = new SuiClient({
     url: getFullnodeUrl("mainnet"),
@@ -33,5 +33,43 @@ const readTable = async function (tableId: string) {
     });
 
 }
+
+
+async function getDynamicFieldsOfObject(
+    objectId: string
+) {
+
+    client.getDynamicFields({
+        parentId: objectId
+    }).then(fields => {
+
+        const allFields = fields.data;
+        console.log("| All Fields |: ", allFields.length);
+
+        //iterate over all fields
+        allFields.forEach(field => {
+
+            client
+                .getObject({ id: field.objectId, options: { showContent: true  } })
+                .then((res) => {
+                    console.log("Dynamic field: ", res.data.objectId, " Fields : ");
+                    if (res.data.content.dataType == "moveObject") {
+                        const dynamicFieldObjectFields = res.data.content.fields; // id, name , value
+
+                        if(typeof dynamicFieldObjectFields["name"] == "object"){  //for complex name types eg DynField Name = 0x2::kiosk::Listing
+                            console.log("\t " + dynamicFieldObjectFields["name"].type + "( id: " + dynamicFieldObjectFields["name"].fields.id + " ) = "+ "`" + dynamicFieldObjectFields["value"] + "`");
+                        }
+                        else  //for simple names (eg: string)
+                        {
+                            console.log("\t " + dynamicFieldObjectFields["name"] + ": `" + dynamicFieldObjectFields["value"] + "`");
+                        }
+                    }
+                });
+        });
+    });
+}
+
+getDynamicFieldsOfObject("0x24c0247fb22457a719efac7f670cdc79be321b521460bd6bd2ccfa9f80713b14");
+
 
 readTable('0x37f60eb2d9d227949b95da8fea810db3c32d1e1fa8ed87434fc51664f87d83cb');
